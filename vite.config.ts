8754c43323c696ddca09234cfb1e385181ca3f0e
OMGiTzPomPom <pauline.srifi@gmail.com>
2026-09-17 14:51:35 +0200

    fixing https errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-    // Charge les variables d'environnement du fichier .env
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
@@ -14,14 +12,12 @@ export default defineConfig(({ mode }) => {
         ],
         server: {
             proxy: {
-                // Proxy pour les données API (players, etc.)
                 '/api/players': {
                     target: env.VITE_MAP_URL,
                     changeOrigin: true,
                     secure: false,
                     rewrite: (path) => path.replace(/^\/api\/players/, '/tiles/players.json'),
                 },
-                // Proxy pour récupérer les tuiles (images) de la carte
                 '/tiles': {
                     target: env.VITE_MAP_URL,
                     changeOrigin: true,
